c8bcdce124a1458d3c592291e9e532f616fbb9d7
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-29 16:49:08 +0300

    Add files via upload

--- CreateUser.js
+++ CreateUser.js
@@ -1,22 +1,34 @@
 import Card from "../UserInterface/Card";
 import Button from "../UserInterface/Button";
-
 import './CreateUser.css';
+import { useState } from "react";
 
 function CreateUser(){
 
+    const [inputName, setInputName] = useState('');
+    const [inputAge, setInputAge] = useState('');
 
     const createUserHandler = (event) => {
         event.preventDefault();
+
+        console.log(inputName, inputAge)
+    }
+
+    const inputNameHandler = (event) => {
+        setInputName(event.target.value);
+    }
+
+    const inputAgeHandler = (event) => {
+        setInputAge(event.target.value);
     }
 
     return(
         <Card className="input">
             <form onSubmit={createUserHandler}>
                 <label htmlFor="name">Имя</label>
-                <input id="name" type="text" />
+                <input id="name" type="text" onChange={inputNameHandler}/>
                 <label htmlFor="age">Возраст</label>
-                <input id="age" type="number" />
+                <input id="age" type="number" onChange={inputAgeHandler}/>
                 <Button type={'submit'}/>
             </form>
         </Card>
